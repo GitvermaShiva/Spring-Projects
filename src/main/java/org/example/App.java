714c23dc03b84e7855b51e141fb77ead39d24f0f
GitvermaShiva <shivamverma63970@gmail.com>
2025-08-01 22:56:04 +0530

    Imported ApplicationContext interface in the main app

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,9 +1,14 @@
 package org.example;
 
-public class App 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        ApplicationContext context=new ClassPathXmlApplicationContext("spring.xml");
+        Dev obj=context.getBean(Dev.class);
+        obj.build();
     }
 }
