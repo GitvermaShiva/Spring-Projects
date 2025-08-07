157e1d607dc93b28d292ef3e412bca3e6193198c
GitvermaShiva <shivamverma63970@gmail.com>
2025-08-07 18:05:50 +0530

    Defined the service class as UserService.java

--- MyWeb/src/main/java/com/shivam/MyWeb/Service/UserService.java
+++ MyWeb/src/main/java/com/shivam/MyWeb/Service/UserService.java
@@ -1,5 +1,21 @@
 package com.shivam.MyWeb.Service;
 
+import com.shivam.MyWeb.Model.User;
+
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Arrays;
+
+@Service
 public class UserService {
+
+    public List<User> getAllUsers(){
+        return Arrays.asList(
+            new User(1,"Shivam","shivam@gmail.com"),
+            new User(2,"Raj","raj@gmail.com"),
+            new User(3,"Rajesh","rajesh@gmail.com")
+        );
+    }
 
 }
