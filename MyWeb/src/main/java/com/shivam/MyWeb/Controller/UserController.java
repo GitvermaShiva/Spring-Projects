6559f91676e07fc9780ff4c16b14d5821535def3
GitvermaShiva <shivamverma63970@gmail.com>
2025-08-07 18:06:27 +0530

    Defined the controller class as UserController.java

--- MyWeb/src/main/java/com/shivam/MyWeb/Controller/UserController.java
+++ MyWeb/src/main/java/com/shivam/MyWeb/Controller/UserController.java
@@ -1,5 +1,23 @@
 package com.shivam.MyWeb.Controller;
 
+import com.shivam.MyWeb.Model.User;
+import com.shivam.MyWeb.Service.UserService;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
 public class UserController {
+
+    @Autowired
+    private UserService userService;
+
+    @RequestMapping("/users")
+    public List<User> getAllUsers(){
+       return userService.getAllUsers();
+    }
 
 }
