c2d684fefbf9b8812c84045a4e8bc3647267ea39
GitvermaShiva <shivamverma63970@gmail.com>
2025-08-11 14:12:48 +0530

    Created a getUserById method

--- MyWeb/src/main/java/com/shivam/MyWeb/Controller/UserController.java
+++ MyWeb/src/main/java/com/shivam/MyWeb/Controller/UserController.java
@@ -3,9 +3,12 @@ package com.shivam.MyWeb.Controller;
 import com.shivam.MyWeb.Model.User;
 import com.shivam.MyWeb.Service.UserService;
 
+import org.springframework.web.bind.annotation.GetMapping;
+//import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.PathVariable;
 
 import java.util.List;
 
@@ -19,5 +22,15 @@ public class UserController {
     public List<User> getAllUsers(){
        return userService.getAllUsers();
     }
+
+    @GetMapping("/users/{id}")
+    public User getUserById(@PathVariable int id){
+        return userService.getUser(id);
+    }
+
+    // @PostMapping()
+    // public void addUser(new User()){
+    //     userService.addUser();
+    // }
 
 }
