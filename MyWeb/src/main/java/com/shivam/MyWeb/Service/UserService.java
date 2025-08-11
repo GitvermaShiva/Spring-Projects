0aba3327853961825f2d31487800c004b364b9ae
GitvermaShiva <shivamverma63970@gmail.com>
2025-08-11 14:14:12 +0530

    Defined a getUser method

--- MyWeb/src/main/java/com/shivam/MyWeb/Service/UserService.java
+++ MyWeb/src/main/java/com/shivam/MyWeb/Service/UserService.java
@@ -5,17 +5,26 @@ import com.shivam.MyWeb.Model.User;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.ArrayList;
 import java.util.Arrays;
 
 @Service
 public class UserService {
 
+    List<User> users=new ArrayList<>(Arrays.asList(new User(1,"Shivam","shivam@gmail.com"),new User(2,"Raj","raj@gmail.com"), new User(3,"Rajesh","rajesh@gmail.com")));
+
     public List<User> getAllUsers(){
-        return Arrays.asList(
-            new User(1,"Shivam","shivam@gmail.com"),
-            new User(2,"Raj","raj@gmail.com"),
-            new User(3,"Rajesh","rajesh@gmail.com")
-        );
+        return users;
+    }
+
+    public User getUser(int userId){
+        return users.stream()
+                    .filter(u -> u.getId() == userId)
+                    .findFirst().get();
+    }    
+
+    public void addUser(User user){
+
     }
 
 }
